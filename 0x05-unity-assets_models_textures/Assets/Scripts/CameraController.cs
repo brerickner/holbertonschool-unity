fea5cb7cb332b82456361ba447e296d8c66f7dd7
Bre <49533377+brerickner@users.noreply.github.com>
2021-08-31 16:34:46 -0500

    camera angle rotating properly around player now

--- 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -6,18 +6,42 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-
+    public float rotateSpeed = 5f;
+    public Transform pivot;
+    public bool useOffsetValues;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        offset = target.position - transform.position;
-    }
+    void Start() {
+        if (!useOffsetValues)
+        {
+            // target = GetComponent<Transform>();
+            offset = target.position - transform.position;
+        }
 
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position - offset;
+        // Get the x position of the mouse and rotate the target
+        // rotate player within the world
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        // Get Y position of mouse and rotate pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        // Move camera based on the current rotation of the target and the original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        // transform.position = target.position - offset;
         transform.LookAt(target);
     }
 }
