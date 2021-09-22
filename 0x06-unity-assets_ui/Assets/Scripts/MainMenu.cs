dd3adb924bd2fd58800573fdbc2aea756dc69614
Bre <49533377+brerickner@users.noreply.github.com>
2021-09-22 15:14:22 -0500

    switching scenes like a boss

--- 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
 {
 
     public void LevelSelect(int level) {
-        SceneManager.LoadScene(level);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + level);
         // PlayerPrefs.GetInt("Scene", level);
 
     }
