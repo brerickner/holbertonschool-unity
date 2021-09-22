9ea83a226e681e31c620abe5b853654f7870c38d
Bre <49533377+brerickner@users.noreply.github.com>
2021-09-22 15:06:58 -0500

    adding progress. option scene works

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void LevelSelect(int level) {
+        SceneManager.LoadScene(level);
+        // PlayerPrefs.GetInt("Scene", level);
+
+    }
+    public void OptionsButton() 
+    {
+        SceneManager.LoadScene(4);
+    }
+    // public void ExitButton(){
+    //     Application.Quit();
+    //     Debug.Log("Exited");
+    // }
+}
