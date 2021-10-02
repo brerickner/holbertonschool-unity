9663eab8ad824cdf2544d50994834576767bad8a
Bre <49533377+brerickner@users.noreply.github.com>
2021-10-02 12:28:47 -0500

    adding progress flipping shapes

--- 2D-clone/Assets/Scripts/TetrisBlock.cs
+++ 2D-clone/Assets/Scripts/TetrisBlock.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour
 {
+    public Vector3 rotationPoint;
     private float previousTime;
     public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -16,23 +19,53 @@ public class TetrisBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        // Move left and right
+        // Move left and right after checking if valid move
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if(!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
         }
+
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+            if(!ValidMove())
+                transform.position -= new Vector3(1, 0, 0);
         }
-
+        
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+            if(!ValidMove())
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+        }
+    
         // Falling pieces, faster when key held down
         if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
+            if(!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
             // After piece has fallen
             previousTime = Time.time;
         }
 
+    }
+
+    // Make sure pieces are on the grid
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
