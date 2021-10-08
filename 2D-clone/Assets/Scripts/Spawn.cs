8a32e4d2a13d5502b426cbe4fa156b567f329272
Bre <49533377+brerickner@users.noreply.github.com>
2021-10-08 09:19:57 -0500

    adding progress

--- 2D-clone/Assets/Scripts/Spawn.cs
+++ 2D-clone/Assets/Scripts/Spawn.cs
@@ -1,24 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class Spawn : MonoBehaviour
-{
-    public GameObject[] Tetrominoes;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        NewTetrimino();
+using System.Collections;
+ 
+public class Spawn : MonoBehaviour {
+ 
+    float fall = 0f;
+    public float fallSpeed = 1f;
+ 
+    // Use this for initialization
+    void Start() {
+   
     }
-
-    public void NewTetrimino()
-    {
-        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
-    }
+   
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        CheckUserInput();
+    }
+ 
+    void CheckUserInput() 
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+            transform.position += new Vector3(1, 0, 0);
+ 
+            if (CheckIsValidPosition()) 
+            {
+            } 
+            else 
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+ 
+        } 
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        {
+            transform.position += new Vector3(-1, 0, 0);
+ 
+            if (CheckIsValidPosition()) 
+            {    
+            }
+            else 
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+       
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            transform.Rotate(0, 0, 90);
+ 
+            if (CheckIsValidPosition()) 
+            {   
+            } 
+            else 
+            {
+                transform.Rotate (0, 0, -90);
+            }
+           
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed) 
+        {
+            transform.position += new Vector3(0, -1, 0);
+ 
+            if (CheckIsValidPosition()) 
+            {
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+            fall = Time.time;  
+        }
+    }
+ 
+    bool CheckIsValidPosition() 
+    {
+        foreach (Transform mino in transform) 
+        {
+            Vector2 pos = FindObjectOfType<TetrisBlock>().Round(mino.position);
+ 
+            if (FindObjectOfType<TetrisBlock>().CheckIsInsideGrid(pos) == false) 
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- 2D-clone/Assets/Scripts/TetrisBlock.cs
+++ 2D-clone/Assets/Scripts/TetrisBlock.cs
@@ -1,95 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class TetrisBlock : MonoBehaviour
+using System.Collections;
+public class TetrisBlock : MonoBehaviour 
 {
-    public Vector3 rotationPoint;
-    private float previousTime;
-    public float fallTime = 0.8f;
-    public static int height = 20;
-    public static int width = 10;
-    public static Transform[,] grid = new Transform[width, height];
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+ 
+    // Use this for initialization
+    void Start () {
+   
     }
-
+   
     // Update is called once per frame
-    void Update()
-    {   
-        // Move left and right after checking if valid move
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-            if(!ValidMove())
-                transform.position -= new Vector3(-1, 0, 0);
-        }
-
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(1, 0, 0);
-            if(!ValidMove())
-                transform.position -= new Vector3(1, 0, 0);
-        }
-
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
-            if(!ValidMove())
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
-        }
-    
-        // Falling pieces, faster when key held down
-        if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
-        {
-            transform.position += new Vector3(0, -1, 0);
-            if(!ValidMove())
-            {
-                // Disable script then spawn new tetrimino upon hitting the bottom
-                transform.position -= new Vector3(0, -1, 0);
-                AddToGrid();
-                this.enabled = false;
-                FindObjectOfType<Spawn>().NewTetrimino();
-            }
-            // After piece has fallen
-            previousTime = Time.time;
-        }
-
+    void Update () {
+   
     }
-
-    void AddToGrid()
+    public bool CheckIsInsideGrid(Vector2 pos) 
     {
-        foreach (Transform children in transform)
-        {
-            int roundedX = Mathf.RoundToInt(children.transform.position.x);
-            int roundedY = Mathf.RoundToInt(children.transform.position.y);
-            
-            grid[roundedX, roundedY] = children;
-        }
+        return((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
     }
-    // Make sure pieces are on the grid
-    bool ValidMove()
+ 
+    public Vector2 Round(Vector2 pos) 
     {
-        foreach (Transform children in transform)
-        {
-            int roundedX = Mathf.RoundToInt(children.transform.position.x);
-            int roundedY = Mathf.RoundToInt(children.transform.position.y);
-            Debug.Log("roundedY: " + Mathf.RoundToInt(children.transform.position.y));
-            Debug.Log("height: " + height);
-            Debug.Log("roundedX: " + roundedX);
-            Debug.Log("width: " + width);
-
-            if(roundedX < 0 || roundedX > width || roundedY < 1 || roundedY > height)
-            {
-                return false;
-            }
-            // if grid spot taken up by another square
-            if (grid[roundedX, roundedY] != null)
-                return false;
-        }
-        return true;
+        return(new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y)));
     }
 }
